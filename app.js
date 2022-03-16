3024801ab1ccaa22eb699ec27dd618fd915ac58a
MESSOUSSI <ines.messoussi@ecl21.ec-lyon.fr>
2022-03-16 23:53:03 +0100

    'image_player'

--- app.js
+++ app.js
@@ -47,8 +47,15 @@ app.get('/video', function (req, res) {
   const videoStream = fs.createReadStream(videoPath, { start, end });
   videoStream.pipe(res);
 });
+// charger image
+
 
 server.listen(3000, () => {
   console.log('listening on *:3000');
 });
 module.exports = app;
+
+
+
+
+
